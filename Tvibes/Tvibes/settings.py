80c2928a130532a894df86ae4c73dbc79419216a
kingsleyabonyi <kinabonyi@gmail.com>
2023-01-04 05:45:22 +0100

    to activate through email - fifth commit

--- Tvibes/Tvibes/settings.py
+++ Tvibes/Tvibes/settings.py
@@ -12,8 +12,16 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'tvibes23@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_PORT = 587
+
 
 
 # Quick-start development settings - unsuitable for production
--- Tvibes/login/views.py
+++ Tvibes/login/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from Tvibes import settings
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -19,13 +21,41 @@ def createaccount(request):
         password1 = request.POST.get('password1')
         password2 = request.POST.get('password2')
 
+        if User.objects.filter(username=username):
+            messages.error(request, 'username already exist! please try other username')
+            return redirect('home')
+
+        if User.objects.filter(email=email):
+            messages.error(request, 'Email already registered')
+            return redirect('home')
+
+        if len(username)>10:
+            messages.error(request, 'Username must be under 10 characters')
+            # return redirect('home')
+
+        if password1 != password2:
+            messages.error(request, 'Passwords did not match')
+            # return redirect('home')
+
+        if not username.isalnum():
+            messages.error(request, 'Username must be Alpha-Numeric')
+            return redirect('home')
+
         myuser = User.objects.create_user(username, email, password1)
         myuser.first_name = fname
         myuser.last_name = lname
 
         myuser.save()
 
-        messages.success(request, "Your account has been successfully created")
+        messages.success(request, "Your account has been successfully created, we have sent you a confirmation email, please check and activate your account.")
+
+        # Welcome email
+
+        subject = 'Welcome to T-Vibes!'
+        message = 'Hello' + myuser.first_name + '!! n/' + 'Welcome to T-Vibes n/ Thank you for choosing us.n/ We have also sent you a confirmation email, please confirm your email address in order to activate your account!.n/n/ Thank you!'
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
 
         return redirect('login')
 
